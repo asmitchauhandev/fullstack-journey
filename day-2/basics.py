1bb302e248266bf5b559a6474c2ac888d58b11b5
Asmit Chauhan <asmitchauhan66@gmail.com>
2026-01-31 12:27:10 +0530

    Day 2: Python basics

---
+++ day-2/basics.py
@@ -0,0 +1,43 @@
+# --------------------
+# Day 2: Variables & Data Types
+# --------------------
+name = "Asmit"
+age = 22
+learning = True
+
+print(name)
+print(age)
+print(learning)
+
+# --------------------
+# User Input
+# --------------------
+name = input("Enter your name: ")
+print("Welcome", name)
+
+# --------------------
+# Type Conversion
+# --------------------
+age = int(input("Enter your age: "))
+print("You will be", age + 1, "next year")
+
+# --------------------
+# Operators
+# --------------------
+a = 10
+b = 3
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+
+# --------------------
+# Mini Task
+# --------------------
+name = input("Enter your name: ")
+goal = input("What are you learning? ")
+
+print("My name is", name)
+print("I am learning", goal)
+print("I will become a Full Stack Developer")
